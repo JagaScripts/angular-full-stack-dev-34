bbfe735c4a79cf0d241a806cfb2ccea36a75ea8e
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-08 23:45:58 +0200

    Calculadora terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-full-stack-dev-u34';
+
+  resultado = "";
+  mensaje = "";
+
+
+  calcular() {
+
+    try {
+
+      this.resultado = eval(this.resultado);
+
+    } catch (error: any) {
+
+      this.mensaje = "La expresión introducida no se puede evaluar, intentalo de nuevo pulsa el boton C";
+
+    }
+
+  }
+
+  generarExpresion(id: string) {
+
+    this.resultado += `${id}`;
+
+  }
+
+  limpiar() {
+
+    this.resultado = "";
+  }
 }
